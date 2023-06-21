75a8dfe1daccc29fa96ceca7b286a3396e30db27
Vladimir <vermakov410@gmail.com>
2023-06-22 01:18:52 +0300

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M 
+// чисел. Посчитайте, сколько чисел больше 0 ввёл 
+// пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int [] InputArray(int length)
+{
+    int [] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Promt($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = Promt("Введвите колличество элементов >");
+int [] array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Колличество чисел больше 0 - {CountPositiveNumbers(array)}");
+
+
